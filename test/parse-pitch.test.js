516ccbb6c913e2b9f845f3650e5ae59179974363
John Leszczynski <jrleszczynski@gmail.com>
2015-08-21 06:35:17 -0400

    change indentation to 2 spaces

--- test/parse-pitch.test.js
+++ test/parse-pitch.test.js
@@ -2,59 +2,59 @@ var test = require('tape')
 var parsePitch = require('../lib/parse-pitch.js')
 
 test('pitch parsing works for notes without accidentals', function(t) {
-    var pitch_F4 = {
-               letter: 'F',
-           accidental: '',
-        numAccidental: 0,
-               octave: 4,
-             sciPitch: 'F4'
-    }
+  var pitch_F4 = {
+           letter: 'F',
+       accidental: '',
+    numAccidental: 0,
+           octave: 4,
+         sciPitch: 'F4'
+  }
 
-    t.deepEqual(parsePitch('F4'), pitch_F4)
-    t.deepEqual(parsePitch('f4'), pitch_F4, "capitalizes letter")
-    t.deepEqual(parsePitch('f'),  pitch_F4, "defaults to octave 4")
-    t.notDeepEqual(parsePitch('c'), pitch_F4)
+  t.deepEqual(parsePitch('F4'), pitch_F4)
+  t.deepEqual(parsePitch('f4'), pitch_F4, "capitalizes letter")
+  t.deepEqual(parsePitch('f'),  pitch_F4, "defaults to octave 4")
+  t.notDeepEqual(parsePitch('c'), pitch_F4)
 
-    t.end()
+  t.end()
 })
 
 
 test('pitch parsing works for notes with accidentals', function(t) {
-    var pitch_Bb3 = {
-               letter: 'B',
-           accidental: 'b',
-        numAccidental: -1,
-               octave: 3,
-             sciPitch: 'Bb3'
-    }
+  var pitch_Bb3 = {
+           letter: 'B',
+       accidental: 'b',
+    numAccidental: -1,
+           octave: 3,
+         sciPitch: 'Bb3'
+  }
 
-    t.deepEqual(parsePitch('Bb3'), pitch_Bb3)
-    t.deepEqual(parsePitch('bb3'), pitch_Bb3, "capitalizes letter")
-    t.notDeepEqual(parsePitch('Bbb3'), pitch_Bb3)
+  t.deepEqual(parsePitch('Bb3'), pitch_Bb3)
+  t.deepEqual(parsePitch('bb3'), pitch_Bb3, "capitalizes letter")
+  t.notDeepEqual(parsePitch('Bbb3'), pitch_Bb3)
 
-    t.end()
+  t.end()
 })
 
 
 test('pitch parsing returns false for non-pitch strings', function(t) {
-    t.equal(parsePitch(''), false, "returns false")
-    t.equal(parsePitch('5'), false, "returns false")
-    t.equal(parsePitch('Eeb5'), false, "returns false")
-    t.equal(parsePitch('M6'), false, "returns false")
-    t.equal(parsePitch('hello'), false, "returns false")
-    t.equal(parsePitch('Abbbbbbbbbb4'), false, "returns false")
-    t.equal(parsePitch('#F#'), false, "returns false")
-    t.equal(parsePitch('P4#'), false, "returns false")
-    t.equal(parsePitch('bA4'), false, "returns false")
+  t.equal(parsePitch(''), false, "returns false")
+  t.equal(parsePitch('5'), false, "returns false")
+  t.equal(parsePitch('Eeb5'), false, "returns false")
+  t.equal(parsePitch('M6'), false, "returns false")
+  t.equal(parsePitch('hello'), false, "returns false")
+  t.equal(parsePitch('Abbbbbbbbbb4'), false, "returns false")
+  t.equal(parsePitch('#F#'), false, "returns false")
+  t.equal(parsePitch('P4#'), false, "returns false")
+  t.equal(parsePitch('bA4'), false, "returns false")
 
-    t.end()
+  t.end()
 }) 
 
 test('throws an error when given non-string arguments', function(t) {
-    t.throws(parsePitch({}))
-    t.throws(parsePitch(5))
-    t.throws(parsePitch(['Db4']))
-    t.throws(parsePitch(undefined))
+  t.throws(parsePitch({}))
+  t.throws(parsePitch(5))
+  t.throws(parsePitch(['Db4']))
+  t.throws(parsePitch(undefined))
 
-    t.end()
+  t.end()
 }) 
--- test/pitch.test.js
+++ test/pitch.test.js
@@ -3,56 +3,56 @@ var Pitch = require('../lib/pitch.js')
 
 test('new Pitches can be created', function(t) {
 
-    var pitch_Bb4 = new Pitch('Bb4')
+  var pitch_Bb4 = new Pitch('Bb4')
 
-    t.equal(pitch_Bb4.name, 'Bb4')
-    t.true(pitch_Bb4 instanceof Pitch, 'is instance of Pitch')
+  t.equal(pitch_Bb4.name, 'Bb4')
+  t.true(pitch_Bb4 instanceof Pitch, 'is instance of Pitch')
 
-    pitch_Bb4.name = 'E4'
-    t.equal(pitch_Bb4.name, 'Bb4', 'Pitch is immutable')
+  pitch_Bb4.name = 'E4'
+  t.equal(pitch_Bb4.name, 'Bb4', 'Pitch is immutable')
 
-    pitch_Bb4.test = 'testing'
-    t.equal(typeof pitch_Bb4.test, 'undefined', 'Pitch is immutable')
+  pitch_Bb4.test = 'testing'
+  t.equal(typeof pitch_Bb4.test, 'undefined', 'Pitch is immutable')
 
-    t.end()
+  t.end()
 })
 
 
 test('Pitch.toString() generates the correct string', function(t) {
-    t.equal((new Pitch('Bb5')).toString(), 'Bb5')
+  t.equal((new Pitch('Bb5')).toString(), 'Bb5')
 
-    var harmonic = 'C#3 D#3 E3 F#3 G#3 A3 B#3 C#4'
-    var mapped = harmonic.split(' ').map(function(pitch) {
-        return new Pitch(pitch)
-    }).join(' ')
-    t.equal(mapped, harmonic, "toString is being called automatically")
+  var harmonic = 'C#3 D#3 E3 F#3 G#3 A3 B#3 C#4'
+  var mapped = harmonic.split(' ').map(function(pitch) {
+    return new Pitch(pitch)
+  }).join(' ')
+  t.equal(mapped, harmonic, "toString is being called automatically")
 
-    t.end()
+  t.end()
 })
 
 
 test('Pitch prototype parsing methods', function(t) {
-    var pitch_Bbb2 = new Pitch('Bbb2')
+  var pitch_Bbb2 = new Pitch('Bbb2')
 
-    t.equal(pitch_Bbb2.sciPitch(),  'Bbb2', 'Pitch.sciPitch() returns correctly')
-    t.equal(pitch_Bbb2.letter(),       'B', 'Pitch.letter() returns correctly')
-    t.equal(pitch_Bbb2.accidental(),  'bb', 'Pitch.accidental() returns correctly')
-    t.equal(pitch_Bbb2.octave(),         2, 'Pitch.octave() returns correctly')
-    t.equal(pitch_Bbb2.pitchClass(), 'Bbb', 'Pitch.pitchClass() returns correctly')
+  t.equal(pitch_Bbb2.sciPitch(),  'Bbb2', 'Pitch.sciPitch() returns correctly')
+  t.equal(pitch_Bbb2.letter(),       'B', 'Pitch.letter() returns correctly')
+  t.equal(pitch_Bbb2.accidental(),  'bb', 'Pitch.accidental() returns correctly')
+  t.equal(pitch_Bbb2.octave(),         2, 'Pitch.octave() returns correctly')
+  t.equal(pitch_Bbb2.pitchClass(), 'Bbb', 'Pitch.pitchClass() returns correctly')
 
-    var pitch_D = new Pitch('D')
-    t.equal(pitch_D.accidental(), '', 'Pitch.accidental() returns correctly with empty string')
+  var pitch_D = new Pitch('D')
+  t.equal(pitch_D.accidental(), '', 'Pitch.accidental() returns correctly with empty string')
 
-    t.end()
+  t.end()
 })
 
 
 test('Pitch.numAccidental() returns the correct number', function(t) {
-    t.equal((new Pitch('A##9')).numAccidental(), 2)
-    t.equal((new Pitch('G#2')).numAccidental(), 1)
-    t.equal((new Pitch('F4')).numAccidental(), 0)
-    t.equal((new Pitch('Bb3')).numAccidental(), -1)
-    t.equal((new Pitch('Dbb7')).numAccidental(), -2)
+  t.equal((new Pitch('A##9')).numAccidental(), 2)
+  t.equal((new Pitch('G#2')).numAccidental(), 1)
+  t.equal((new Pitch('F4')).numAccidental(), 0)
+  t.equal((new Pitch('Bb3')).numAccidental(), -1)
+  t.equal((new Pitch('Dbb7')).numAccidental(), -2)
 
-    t.end()
+  t.end()
 })
