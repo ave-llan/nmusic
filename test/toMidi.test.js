2c2fe6266bda6da7237ebdabebe669b0829bd44a
John Leszczynski <jrleszczynski@gmail.com>
2015-08-20 21:43:46 -0400

    create toMidi function

---
+++ test/toMidi.test.js
@@ -0,0 +1,16 @@
+var test = require('tape')
+var toMidi = require('../lib/toMidi.js')
+
+
+test('toMidi returns the correct number', function(t) {
+
+
+    t.equal(toMidi('C4'), 60)
+    t.equal(toMidi('B#3'), 60)
+    t.equal(toMidi('Cb4'), 59)
+    t.equal(toMidi('B3'), 59)
+    t.equal(toMidi('A##3'), 59)
+    t.equal(toMidi('Bb3'), 58)
+
+    t.end()
+})
