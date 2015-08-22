e11250dce5c0f2529d290ffb553cc04a869b10d2
John Leszczynski <jrleszczynski@gmail.com>
2015-08-22 08:39:34 -0400

    add functionality to process negative simple intervals

--- test/pitch/plus-interval.test.js
+++ test/pitch/plus-interval.test.js
@@ -20,3 +20,23 @@ test('plusInterval works for positive simple intervals', function (t) {
 
   t.end()
 })
+
+test('plusInterval works for negative simple intervals', function (t) {
+  t.equal(plusInterval('C4', -1), 'C4')
+  t.equal(plusInterval('C4', -2), 'B3')
+  t.equal(plusInterval('C#4', -3), 'A3')
+  t.equal(plusInterval('Cb4', -4), 'G3')
+  t.equal(plusInterval('C##4', -5), 'F3')
+  t.equal(plusInterval('Cbb4', -6), 'E3')
+  t.equal(plusInterval('C#4', -7), 'D3')
+  t.equal(plusInterval('Cb4', -8), 'C3')
+
+  t.equal(plusInterval('D4', -2), 'C4')
+  t.equal(plusInterval('D4', -5), 'G3')
+  t.equal(plusInterval('D4', -10), 'B2')
+  t.equal(plusInterval('A3', -3), 'F3')
+  t.equal(plusInterval('A3', -12), 'D2')
+  t.equal(plusInterval('F5', -5), 'B4')
+
+  t.end()
+})
