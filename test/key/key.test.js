3a1c3bb6c69e87e547231659d18a101aa7bbfebd
John Leszczynski <jrleszczynski@gmail.com>
2015-09-11 17:18:36 -0400

    add tests for .scaleDeggree

--- test/key/key.test.js
+++ test/key/key.test.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var Key = require('../../lib/key/key.js')
 var Pitch = require('../../lib/pitch/pitch.js')
 
-test('Key methods', function (t) {
+test('Key constructor', function (t) {
   var pitch_Bb4 = new Pitch('Bb4')
   t.true(Key('Bb', 'major') instanceof Key)
   t.true(Key(pitch_Bb4, 'minor') instanceof Key)
@@ -10,6 +10,22 @@ test('Key methods', function (t) {
   t.equal(Key('Bb4', 'Major').toString(), 'Bb major')
   t.equal(String(new Key('Bb4', 'DORIAN')), 'Bb dorian')
   t.equal(String(Key('a', ['P1', 'M2', 'M3', 'A4', 'A5', 'A6'])), 'A custom-scale')
+
+  t.end()
+})
+
+test('Key.scaleDegree()', function (t) {
+  var a_major = new Key('A3', 'major')
+  t.true(a_major.scaleDegree(1).equals('A3'))
+  t.true(a_major.scaleDegree(3).equals('C#4'))
+  t.true(a_major.scaleDegree(9).equals('B4'))
+  t.true(a_major.scaleDegree(10).equals('C#5'))
+
+  var eb_dorian = new Key('Eb5', 'dorian')
+  t.true(eb_dorian.scaleDegree(1).equals('Eb5'))
+  t.true(eb_dorian.scaleDegree(3).equals('Gb5'))
+  t.true(eb_dorian.scaleDegree(6).equals('C6'))
+  t.true(eb_dorian.scaleDegree(9).equals('F6'))
 
   t.end()
 })
