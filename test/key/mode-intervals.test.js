264b24fce3c6e63f21280a9d85daa0e0f49f0e9e
John Leszczynski <jrleszczynski@gmail.com>
2015-09-11 15:31:12 -0400

    add test for mode-intervals

---
+++ test/key/mode-intervals.test.js
@@ -0,0 +1,12 @@
+var test = require('tape')
+var modeIntervals = require('../../lib/key/mode-intervals.js')
+
+test('modeIntervals()', function (t) {
+  var major = ['P1', 'M2', 'M3', 'P4', 'P5', 'M6', 'M7']
+
+  t.deepEqual(modeIntervals('major'), major)
+  t.deepEqual(modeIntervals('MAJOR'), major)
+  t.throws(function () {modeIntervals('not_a_mode')})
+
+  t.end()
+})
